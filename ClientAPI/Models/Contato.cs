96a4f573423042a1884c55e695a1bc43733a2a4c
BrunoCardoso02 <bruno.cardoso02@outlook.com>
2025-04-02 13:36:11 -0300

    feature(models): Adicionando entidade de Contato

---
+++ ClientAPI/Models/Contato.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ClientAPI.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; } 
+        public int DDD { get; set; }
+        public decimal Telefone { get; set; }
+    }
+}
