7d18af65b8d12f5554cc3c9bc8efd2c319e39253
BrunoCardoso02 <bruno.cardoso02@outlook.com>
2025-04-02 13:37:27 -0300

    feature(models): Adicionando entidade de Endereco

---
+++ ClientAPI/Models/Endereco.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ClientAPI.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }  
+        public string CEP { get; set; }  
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Referencia { get; set; }
+    }
+}
